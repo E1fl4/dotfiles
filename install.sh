680a773cc1f6d45987fae8d2a68f5f20b16c9688
Eirik <e1fl4@proton.me>
2026-09-09 17:00:14 +0200

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo pacman -S --needed base-devel git stow zsh neovim npm python-pipx imagemagick awww waybar swaync
+git clone https://github.com/E1fl4/dotfiles.git ~/dotfiles
+cd ~/dotfiles
+stow .
+git clone https://github.com/E1fl4/nvim-config.git ~/.config/nvim
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+mkdir -p ~/Pictures/Wallpapers
+mv ~/dotfiles/wallhaven-wqkgkq_3840x2160.png ~/Pictures/Wallpapers
+pipx install pywal16
+~/.local/bin/wal -t -i ~/Pictures/Wallpapers/wallhaven-wqkgkq_3840x2160.png
+chsh -s /bin/zsh
