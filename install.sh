62066697cd60b011d64e1cee0d60ecab53e932d5
E1fl4 <e1fl4@proton.me>
2026-09-11 20:58:37 +0200

    Zsh: autopair plugin

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ cd ~/dotfiles
 stow .
 git clone https://github.com/E1fl4/nvim-config.git ~/.config/nvim
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
+git clone https://github.com/hlissner/zsh-autopair.git ~/.zsh-autopair
 mkdir -p ~/Pictures/Wallpapers
 mv ~/dotfiles/wallhaven-wqkgkq_3840x2160.png ~/Pictures/Wallpapers
 pipx install pywal16
